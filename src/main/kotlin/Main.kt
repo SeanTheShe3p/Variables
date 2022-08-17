1bb53e3cb86ce21a6d918bce42c10af98dff6946
SeanTheShe3p <swiddowson@students.mchenry.edu>
2022-08-16 22:35:25 -0500

    Comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main(args: Array<String>) {
-    var myByte: Byte = 107
-    var myShort: Short = 30000
-    var myInt = 16
-    var myLong = 80000000L
-    var myFloat = 3.14F
-    var myDouble:Double = 160000000.0
-    var myBoolean = false
+    var myByte: Byte = 107 // 8-bit number -128 to 127
+    var myShort: Short = 30000 // a 16-bit number
+    var myInt = 16 // a 32-bit integer
+    var myLong = 80000000L // double the bits of an int, 64-bit
+    var myFloat = 3.14F // an integer with decimals
+    var myDouble:Double = 160000000.0 // a 64-bit float
+    var myBoolean = false // true or false variable
     myBoolean = myDouble<myLong&&myShort>myInt
     var myChar:Char = 'a'
     println(myBoolean)
