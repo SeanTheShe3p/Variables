bc46ce106b05bbf157f5f976a1e52ed16ad9cae2
SeanTheShe3p <swiddowson@students.mchenry.edu>
2022-08-16 22:19:19 -0500

    Inital Up

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var myByte: Byte = 107
+    var myShort: Short = 30000
+    var myInt = 16
+    var myLong = 80000000L
+    var myFloat = 3.14F
+    var myDouble:Double = 160000000.0
+    var myBoolean = false
+    myBoolean = myDouble<myLong&&myShort>myInt
+    var myChar:Char = 'a'
+    println(myBoolean)
+}
